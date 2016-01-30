05fb0f7df16f11429fdbd37d16a419a01b88d141
Michael Mäder <mike@moik.org>
2016-01-30 11:21:12 +0100

    init of the project on Git from currently running version

---
+++ IRForwarder.ino
@@ -0,0 +1,162 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <IRremote.h>
+#include "RestClient.h"
+
+/* IRremote hex codes for received remote commands */
+#define REMOTE_PLAY      0xffc23d
+#define REMOTE_VOLU      0xffa857
+#define REMOTE_VOLD      0xffe01f
+#define REMOTE_0         0xff6897
+#define REMOTE_1         0xff30cf
+#define REMOTE_2         0xff18e7
+#define REMOTE_3         0xff7a85
+#define REMOTE_4         0xff10ef
+#define REMOTE_5         0xff38c7
+#define REMOTE_6         0xff5aa5
+#define REMOTE_7         0xff42bd
+#define REMOTE_8         0xff4ab5
+#define REMOTE_9         0xff52ad
+#define REMOTE_CHP       0xffe21d
+#define REMOTE_CHM       0xffa25d
+
+const int RECV_PIN = 8;
+IRrecv irrecv(RECV_PIN);
+
+/* Millisecond timer values */
+unsigned long   lastcmd = 0;
+
+RestClient client = RestClient("sonosRESTAPI",5000);
+
+// Pin assignments
+byte greenLedPin = 9;
+
+
+void greenLedOn();
+void greenLedOff();
+
+void setup() {
+  // Setup LEDs
+  pinMode(greenLedPin, OUTPUT);
+
+
+/*
+// Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  // this check is only needed on the Leonardo:
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  Serial.println("serial comm established");
+*/
+  client.dhcp();
+//  Serial.println("Connected and IP received");
+  
+  greenLedOn();
+ 
+  irrecv.enableIRIn(); // Start the receiver
+
+  pinMode(greenLedPin, OUTPUT);
+  greenLedOff();
+  delay(200);
+  greenLedOn();
+  delay(200);
+  greenLedOff();
+  delay(200);
+  greenLedOn();
+  delay(200);
+  greenLedOff();
+  delay(200);
+  greenLedOn();
+  delay(200);
+  greenLedOff();
+  delay(200);
+  greenLedOn();
+  delay(200);
+  greenLedOff();
+}
+
+
+void loop() {
+  String response;
+  decode_results results;
+  int statusCode;
+
+  if (irrecv.decode(&results)) {
+    if (millis() > (lastcmd + 300)) {
+      Serial.println(results.value,HEX);
+      /* compare received IR against known commands */
+      switch (results.value) {
+      case REMOTE_VOLU:
+//        Serial.println("Vol Up");
+        statusCode = client.get("/volUp/", &response);
+        break;
+      case REMOTE_VOLD:
+//      Serial.println("Vol Down");
+        statusCode = client.get("/volDown/", &response);
+        break;
+      case REMOTE_PLAY:
+//        Serial.println("Play");
+        statusCode = client.get("/playpause/", &response);
+        break;
+      case REMOTE_0:
+//        Serial.println("Stop");
+        statusCode = client.get("/stop/", &response);
+        break;
+      case REMOTE_1:
+//        Serial.println("IR 1");
+        statusCode = client.get("/ircode/1", &response);
+        break;
+      case REMOTE_2:
+//        Serial.println("IR 2");
+        statusCode = client.get("/ircode/2", &response);
+        break;
+      case REMOTE_3:
+//        Serial.println("IR 3");
+        statusCode = client.get("/ircode/3", &response);
+        break;
+      case REMOTE_4:
+//        Serial.println("IR 4");
+        statusCode = client.get("/ircode/4", &response);
+        break;
+      case REMOTE_5:
+//        Serial.println("IR 5");
+        statusCode = client.get("/ircode/5", &response);
+        break;
+      case REMOTE_6:
+//        Serial.println("IR 6");
+        statusCode = client.get("/ircode/6", &response);
+        break;
+      case REMOTE_7:
+//        Serial.println("IR 7");
+        statusCode = client.get("/ircode/7", &response);
+        break;
+      case REMOTE_8:
+//        Serial.println("IR 8");
+        statusCode = client.get("/ircode/8", &response);
+        break;
+      case REMOTE_9:
+//        Serial.println("IR 9");
+        statusCode = client.get("/ircode/9", &response);
+        break;
+      default:
+//        Serial.print("Unknown code: 0x");
+//        Serial.println(results.value, HEX);
+        break;
+      }
+    /* store time at which last IR command was processed */
+    lastcmd = millis();
+    }
+    irrecv.resume(); // Receive the next value
+  }
+}
+
+
+void greenLedOn() {
+  digitalWrite(greenLedPin, HIGH);
+}
+
+void greenLedOff() {
+  digitalWrite(greenLedPin, LOW);
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Mike, 01.01.16
+--------------
+
+This is the small code, running on the Arduino. This part only forwards the IR codes to the REST API, running on server 192.168.1.4:5000
+
